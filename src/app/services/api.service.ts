fecf0460b3177f542881efd9be154511603e144d
sparshturkane <rajaturkane99@gmail.com>
2020-11-26 10:01:56 +0530

    setup store and api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,170 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { StoreService } from './store.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  public baseURL: String;
+  constructor(private httpClient: HttpClient, private storeService: StoreService) {
+    this.baseURL = environment.BaseURL
+  }
+
+
+  public register(postParams) {
+
+    this.storeService.setLoaderData('block');
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+    return this.httpClient.post(this.baseURL + 'register', postParams, {
+      headers: headers
+    }).subscribe(
+      (data) => {
+        this.storeService.setRegister(data);
+        this.storeService.setLoaderData('none');
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured category.");
+        } else {
+          this.storeService.setRegister(err.error);
+          this.storeService.setLoaderData('none');
+        }
+      }
+    );
+  }
+
+  public login(postParams) {
+
+    this.storeService.setLoaderData('block');
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+    return this.httpClient.post(this.baseURL + 'register', postParams, {
+      headers: headers
+    }).subscribe(
+      (data) => {
+        this.storeService.setLogin(data);
+        this.storeService.setLoaderData('none');
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured category.");
+        } else {
+          this.storeService.setLogin(err.error);
+          this.storeService.setLoaderData('none');
+        }
+      }
+    );
+  }
+
+  public transaction_get() {
+
+    this.storeService.setLoaderData('block');
+
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.httpClient.get(this.baseURL + 'transaction/', {
+      headers: headers
+    }).subscribe(
+      (data) => {
+        this.storeService.setTransaction(data);
+        this.storeService.setLoaderData('none');
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured category.");
+        } else {
+          this.storeService.setTransaction(err.error);
+          this.storeService.setLoaderData('none');
+        }
+      }
+    );
+  }
+
+  public transaction_create(postParams, token) {
+
+    this.storeService.setLoaderData('block');
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .append('authorization', 'Bearer ' + token);
+
+    return this.httpClient.post(this.baseURL + 'transaction/', postParams, {
+      headers: headers
+    }).subscribe(
+      (data) => {
+        this.storeService.setTransaction(data);
+        this.storeService.setLoaderData('none');
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured category.");
+        } else {
+          this.storeService.setTransaction(err.error);
+          this.storeService.setLoaderData('none');
+        }
+      }
+    );
+  }
+
+
+  public transaction_update(postParams, token) {
+
+    this.storeService.setLoaderData('block');
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .append('authorization', 'Bearer ' + token);
+
+    return this.httpClient.patch(this.baseURL + 'transaction/' + postParams, {
+      headers: headers
+    }).subscribe(
+      (data) => {
+        this.storeService.setTransaction(data);
+        this.storeService.setLoaderData('none');
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured category.");
+        } else {
+          this.storeService.setTransaction(err.error);
+          this.storeService.setLoaderData('none');
+        }
+      }
+    );
+  }
+
+
+  public transaction_delete(postParams, token) {
+
+    this.storeService.setLoaderData('block');
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .append('authorization', 'Bearer ' + token);
+
+    return this.httpClient.delete(this.baseURL + 'transaction/' + postParams, {
+      headers: headers
+    }).subscribe(
+      (data) => {
+        this.storeService.setTransaction(data);
+        this.storeService.setLoaderData('none');
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured category.");
+        } else {
+          this.storeService.setTransaction(err.error);
+          this.storeService.setLoaderData('none');
+        }
+      }
+    );
+  }
+
+
 }
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,56 @@ import { Injectable } from '@angular/core';
 export class StoreService {
 
   constructor() { }
+
+  // REGISTER DATA
+  private registerApiData;
+  public registerApiDataChanged = new Subject<any>();
+
+  getRegister() {
+    return this.registerApiData;
+  }
+
+  setRegister(response) {
+    this.registerApiData = response;
+    this.registerApiDataChanged.next(this.registerApiData);
+  }
+
+  // LOGIN DATA
+  private loginApiData;
+  public loginApiDataChanged = new Subject<any>();
+
+  getLogin() {
+    return this.loginApiData;
+  }
+
+  setLogin(response) {
+    this.loginApiData = response;
+    this.loginApiDataChanged.next(this.loginApiData);
+  }
+
+  // TRANSACTION DATA
+  private transactionApiData;
+  public transactionApiDataChanged = new Subject<any>();
+
+  getTransaction() {
+    return this.transactionApiData;
+  }
+
+  setTransaction(response) {
+    this.transactionApiData = response;
+    this.transactionApiDataChanged.next(this.transactionApiData);
+  }
+
+  // USER LOADER DATA
+  private loaderData;
+  public loaderDataChanged = new Subject<any>();
+
+  getLoaderData() {
+    return this.loaderData;
+  }
+
+  setLoaderData(Response) {
+    this.loaderData = Response;
+    this.loaderDataChanged.next(this.loaderData);
+  }
 }
